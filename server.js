df06e72573d6193b592a7652a3a50f0d25445d96
preservance717 <GL717@outlook.com>
2016-06-06 21:24:33 +0800

    commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+
+var file = require("./condition/isExistFile");
+var routeItems = require("./routes/get_items");
+var routeItem = require("./routes/get_item");
+var routeInsertItem = require("./routes/insert_item");
+var routeDeleteItem = require("./routes/delete_item");
+var routeUpdateItem = require("./routes/update_item");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/items', routeItems);
+app.use('/items', routeItem);
+app.use('/items', routeInsertItem);
+app.use('/items', routeDeleteItem);
+app.use('/items', routeUpdateItem);
+
+
+app.listen(3000);
+
+module.exports = app;
