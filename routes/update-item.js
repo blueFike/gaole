db3bf776b13e2d427a02c1f95a3404f6963d7c9a
preservance717 <GL717@outlook.com>
2016-06-19 20:14:47 +0800

    modified packing

--- routes/update-item.js
+++ routes/update-item.js
@@ -8,7 +8,10 @@ var fixturesFile = './fixtures.json';
 
 router.put('/:id', function (req, res) {
     var id = req.params.id;
+    updateData(res, req, id);
+});
 
+function updateData(res, req, id){
     fs.readFile(fixturesFile, 'UTF-8', function (err, data) {
         var items = JSON.parse(data);
 
@@ -20,19 +23,29 @@ router.put('/:id', function (req, res) {
         for (var i = 0; i < items.length; i++) {
             if (items[i].id === parseInt(id)) {
                 items[i]  = {
-                    id: items[i].id, barcode: req.body.barcode,
-                    name: req.body.name, unit: req.body.unit, price: req.body.price, nextId: items[i].nextId
+                    id: items[i].id,
+                    barcode: req.body.barcode,
+                    name: req.body.name,
+                    unit: req.body.unit,
+                    price: req.body.price
                 };
-                fs.writeFile(fixturesFile, JSON.stringify(items), function () {
-                    res.status(201).json({
-                        id: items[i].id, barcode: req.body.barcode,
-                        name: req.body.name, unit: req.body.unit, price: req.body.price
-                    });
-                });
+                writeData(items, res, req, items[i].id);
                 break;
             }
         }
     });
-});
+}
+
+function writeData(items, res, req, id) {
+    fs.writeFile(fixturesFile, JSON.stringify(items), function () {
+        res.status(201).json({
+            id: id,
+            barcode: req.body.barcode,
+            name: req.body.name,
+            unit: req.body.unit,
+            price: req.body.price
+        });
+    });
+}
 
 module.exports = router;
